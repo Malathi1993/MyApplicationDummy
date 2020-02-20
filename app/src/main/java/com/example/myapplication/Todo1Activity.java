00e75491a148f4a6965854bd1fe3b72dfb74abdc
Malathi <57785954+Malathi1993@users.noreply.github.com>
2020-02-20 14:28:21 +0530

    Modifyy

--- app/src/main/java/com/example/myapplication/Todo1Activity.java
+++ app/src/main/java/com/example/myapplication/Todo1Activity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -16,67 +17,79 @@ import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Todo1Activity extends AppCompatActivity {
-    TextView TaskName;
-    TextView workdiscription;
     TextView date;
-    EditText TODO;
-    EditText Discription;
+    EditText task_name;
+    EditText work_discription;
     //CalendarView calendarView;
-    Button AddTask;
-    Button CalenderPicker;
+    Button calender_picker;
+    Button add_task;
     private int mYear, mMonth, mDay;
-    private View CalendarPicker;
+    private View calendar_dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo1);
 
-        TaskName = (TextView) findViewById(R.id.tv_taskname);
+        task_name = (EditText) findViewById(R.id.et_TaskName);
+        work_discription = (EditText) findViewById(R.id.et_Discription);
         date = (TextView) findViewById(R.id.date);
-        workdiscription = (TextView) findViewById(R.id.tv_workdiscription);
-        TODO = (EditText) findViewById(R.id.et_TaskName);
-        Discription = (EditText) findViewById(R.id.et_Discription);
-        AddTask = (Button) findViewById(R.id.btn_addtask);
-        CalenderPicker = (Button) findViewById(R.id.btn_Calendarpicker);
+        add_task = (Button) findViewById(R.id.btn_addtask);
+        calender_picker = (Button) findViewById(R.id.btn_Calendarpicker);
+        calender_picker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCalenderDialog();
+            }
+        });
 
-
-        CalenderPicker.setOnClickListener((View.OnClickListener) this);
+        add_task.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addTask();
+            }
+        });
     }
 
-              public void onClick(View v) {
-            if (v == CalendarPicker) {
+    public void showCalenderDialog() {
+        // Get Current Date
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
 
-                // Get Current Date
-                final Calendar c = Calendar.getInstance();
-                mYear = c.get(Calendar.YEAR);
-                mMonth = c.get(Calendar.MONTH);
-                mDay = c.get(Calendar.DAY_OF_MONTH);
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
 
-                DatePickerDialog datePickerDialog = new DatePickerDialog(this,
-                        new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
 
-                            @Override
-                            public void onDateSet(DatePicker view, int year,
-                                                  int monthOfYear, int dayOfMonth) {
+                        date.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                        date.setVisibility(View.VISIBLE);
 
-                                CalendarPicker.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.setCancelable(true);
+        datePickerDialog.show();
+    }
 
-                            }
-                        }, mYear, mMonth, mDay);
-                datePickerDialog.show();
-            }
+    public void addTask(){
+        Intent intent = new Intent();
+        Bundle data = new Bundle();
+        data.putString("task_name", task_name.getText().toString());
+        data.putString("task_desc", work_discription.getText().toString());
+        data.putString("date", date.getText().toString());
+        intent.putExtras(data);
+        setResult(RESULT_OK, intent);
+        finish();
+    }
 
-
-
-
-
-            }
-
-            }
+}
 
 
